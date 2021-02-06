33db08e1a5d7c04e699b94f7dc2a00b031a4933c
cascharly <cascharly@gmail.com>
2021-02-06 09:24:27 -0300

    06 Lista de categorias

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
+  // const categories = ["One Punch", "Samurai X", "Dragon Ball"];
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  const handleAdd = () => {
+    setCategories([...categories,'Hunter X'])       // Opcion 1
+    // setCategories( cats=> [...cats,'Hunter X'] )    // Opcion 2
+  };
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+      <button onClick={handleAdd}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
