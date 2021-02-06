0b0f38be4db23ca31e08507daa79b7dbc335a755
cascharly <cascharly@gmail.com>
2021-02-06 17:06:31 -0300

    10 useEffect

--- src/components/GridGrid.js
+++ src/components/GridGrid.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const GridGrid = ({ category }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    getGifs();
+  }, []);
+
   const getGifs = async () => {
     const url =
       "https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=s05N6bkckoYkKh91RrWCtMXEE4uC6c7C";
@@ -18,11 +24,11 @@ export const GridGrid = ({ category }) => {
     console.log(gifs);
   };
 
-  getGifs();
-
   return (
     <>
       <h3>{category}</h3>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>---</button>
     </>
   );
 };
