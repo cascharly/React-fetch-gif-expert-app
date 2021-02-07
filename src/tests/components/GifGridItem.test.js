43693e5f9a98a1b5dceca02c1319ab577b8e801d
cascharly <cascharly@gmail.com>
2021-02-07 13:12:39 -0300

    PropTypes y Snapshots

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,7 +3,10 @@ import { GifGridItem } from "../../components/GifGridItem";
 
 describe("Pruebas en <GifGridItem />", () => {
   test("debe mostrar el componente correctamente", () => {
-    const wrapper = shallow(<GifGridItem />);
+    const title = "Un titulo";
+    const url = "https://localhost/algo.jpg";
+
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
     expect(wrapper).toMatchSnapshot();
   });
