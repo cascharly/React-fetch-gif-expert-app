e1ad235646cb4d0d50ec295c68cc808269142018
cascharly <cascharly@gmail.com>
2021-02-07 13:05:44 -0300

    03 Enzyme test

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,10 @@
+import { shallow } from "enzyme";
+import { GifGridItem } from "../../components/GifGridItem";
+
+describe("Pruebas en <GifGridItem />", () => {
+  test("debe mostrar el componente correctamente", () => {
+    const wrapper = shallow(<GifGridItem />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
